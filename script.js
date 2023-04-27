879a3e472ce57f2c6ea7012f4bdc991bff7cad25
Saravanan <127502394+Sarava-NaN@users.noreply.github.com>
2023-04-27 18:25:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let searchBarEl=document.querySelector(".search_Bar_Icon_Container");
-let magnifierEl=document.querySelector(".magnifier");
-//
-magnifierEl.addEventListener("click",()=>{
-    searchBarEl.classList.toggle("active");
-});
+let lists=document.querySelector(".lists");
+let navBar=document.querySelector(".arrow");
+
+navBar.addEventListener("click",()=>{
+    lists.classList.toggle("javaScript_1");
+    navBar.children[0].classList.toggle("jslink_1");
+    navBar.children[1].classList.toggle("jslink_2");
+    navBar.children[2].classList.toggle("jslink_3");
+})
